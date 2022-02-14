1f06b6737b13bc39d2352647d702261ac1d856d6
Joe Sharp <ratracejoe@hotmail.com>
2022-02-14 10:59:16 +0000

    building hacknet

--- hacknet/keepBuildingHacknet.js
+++ hacknet/keepBuildingHacknet.js
@@ -9,29 +9,30 @@ async function waitForMoney(ns, purpose, cost) {
         await ns.sleep(3000);
     }
 }
-const LEVEL_TARGET = 80;
-const LEVEL_INCREMENT = 5;
+const LEVEL_TARGET = 200;
 const RAM_TARGET = 16;
-const RAM_INCREMENT = 2;
 const CORES_TARGET = 8;
-const CORES_INCREMENT = 1;
 
-async function upgradeNode(ns, index) {
-    while (ns.hacknet.getNodeStats(index).level < LEVEL_TARGET) {
-        await waitForMoney(ns, `Upgrade Level of ${index}`, ns.hacknet.getLevelUpgradeCost(index, LEVEL_INCREMENT));
-        ns.hacknet.upgradeLevel(index, LEVEL_INCREMENT);
+async function upgradeNode(ns, index,
+    targetLevel = LEVEL_TARGET,
+    targetRam = RAM_TARGET,
+    targetCores = CORES_TARGET
+) {
+    while (ns.hacknet.getNodeStats(index).level < targetLevel) {
+        await waitForMoney(ns, `Upgrade Level of ${index}`, ns.hacknet.getLevelUpgradeCost(index, 1));
+        ns.hacknet.upgradeLevel(index, 1);
     };
     ns.print(`Node ${index} Upgraded to Target Level`);
 
-    while (ns.hacknet.getNodeStats(index).ram < RAM_TARGET) {
-        await waitForMoney(ns, `Upgrade RAM of ${index}`, ns.hacknet.getRamUpgradeCost(index, RAM_INCREMENT));
-        ns.hacknet.upgradeRam(index, RAM_INCREMENT);
+    while (ns.hacknet.getNodeStats(index).ram < targetRam) {
+        await waitForMoney(ns, `Upgrade RAM of ${index}`, ns.hacknet.getRamUpgradeCost(index, 1));
+        ns.hacknet.upgradeRam(index, 1);
     };
     ns.print(`Node ${index} Upgraded to Target RAM`);
 
-    while (ns.hacknet.getNodeStats(index).cores < CORES_TARGET) {
-        await waitForMoney(ns, `Upgrade Cores of ${index}`, ns.hacknet.getCoreUpgradeCost(index, CORES_INCREMENT));
-        ns.hacknet.upgradeCore(index, CORES_INCREMENT);
+    while (ns.hacknet.getNodeStats(index).cores < targetCores) {
+        await waitForMoney(ns, `Upgrade Cores of ${index}`, ns.hacknet.getCoreUpgradeCost(index, 1));
+        ns.hacknet.upgradeCore(index, 1);
     };
     ns.print(`Node ${index} Upgraded to Target Cores`);
 }
@@ -47,11 +48,12 @@ async function buildNewNode(ns) {
 export async function main(ns) {
     ns.disableLog("getServerMoneyAvailable");
     ns.disableLog("sleep");
-    while (true) {
-        for (let i = 0; i < ns.hacknet.numNodes(); i++) {
-            await upgradeNode(ns, i);
-        }
 
+    for (let i = 0; i < ns.hacknet.numNodes(); i++) {
+        await upgradeNode(ns, i);
+    }
+
+    while (true) {
         await buildNewNode(ns);
     }
 }
