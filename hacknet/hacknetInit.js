db81edf361b8edf967ab1bde7a3e17b620485da1
Karl Davis <kad@marling.school>
2022-02-16 09:24:17 +0000

    Fixed errors

--- hacknet/hacknetInit.js
+++ hacknet/hacknetInit.js
@@ -1,12 +1,17 @@
+import { bitNodeCheck } from "helpers.js"
+const BIT_NODE = 4
 /** @param {NS} ns **/
 export async function main(ns) {
+	const nwoLvl = ns.getServerRequiredHackingLevel("nwo");
 	// We will not buy anything if there's less money than this ammount. 
-	const reserveMoney = 50000
+	const reserveMoney = 500000
 	// Number of times to upgrade (shouldn't have to change this)
 	const n = 1;
 
 	ns.disableLog("getServerMoneyAvailable");
-	ns.exec("hashControl.js", "home", 1);
+	if (bitNodeCheck(ns, BIT_NODE)){
+		ns.exec("hashControl.js", "home", 1);
+	}
 
 	while (ns.getServerMoneyAvailable("home") < ns.hacknet.getPurchaseNodeCost) {
 		await ns.sleep(10000);
@@ -25,47 +30,49 @@ export async function main(ns) {
 		// If there is not enough money, we wait for it instead of ending 
 		// the loop.
 		buyNewServer(ns);
-		
+
 		while (ns.getServerMoneyAvailable("home") >= reserveMoney) {
 			for (let i = 0; i < ns.hacknet.numNodes(); i++) {
 				while (ns.hacknet.getLevelUpgradeCost(i, n) < Infinity && ns.hacknet.upgradeLevel(i, n)) {
 					ns.print("Upgraded " + ns.hacknet.getNodeStats(i).name + " to level " + ns.hacknet.getNodeStats(i).level);
+					await ns.sleep(0)
+					buyNewServer(ns)
 				}
-				buyNewServer(ns)
-				await ns.sleep(10)
 				while (ns.hacknet.getRamUpgradeCost(i, n) < Infinity && ns.hacknet.upgradeRam(i, n)) {
 					ns.print("Upgraded " + ns.hacknet.getNodeStats(i).name + " RAM to " + ns.hacknet.getNodeStats(i).ram);
-					await ns.sleep(10)
+					await ns.sleep(0)
 					buyNewServer(ns)
-
 				}
 				while (ns.hacknet.getCoreUpgradeCost(i, n) < Infinity && ns.hacknet.upgradeCore(i, n)) {
 					ns.print("Upgraded " + ns.hacknet.getNodeStats(i).name + " core to " + ns.hacknet.getNodeStats(i).core);
-					await ns.sleep(10)
+					await ns.sleep(0)
 					buyNewServer(ns)
 				}
-				if (ns.fileExists("sqlinject.exe")) {
-					if (ns.hacknet.getNodeStats(i).cache < 4) {
-						while (ns.hacknet.getCacheUpgradeCost(i, n) && ns.hacknet.upgradeCache(i, n)
-						) {
+				if (bitNodeCheck(ns, BIT_NODE)) {
+					if (ns.getHackingLevel() > nwoLvl)
+						while (ns.hacknet.getCacheUpgradeCost(i, n) && (ns.hacknet.getNodeStats(i).cache < 5)) {
+							ns.hacknet.upgradeCache(i, n)
 							ns.print("Upgraded " + ns.hacknet.getNodeStats(i).name + " cache to " + ns.hacknet.getNodeStats(i).cache);
-							await ns.sleep(10)
+							await ns.sleep(0)
 							buyNewServer(ns);
 						}
-					}
 				}
-			}
-			await ns.sleep(10)
 
+			}
+			await ns.sleep(0)
 		}
-		await ns.sleep(10)
+		await ns.sleep(0)
 
 
 	}
-	await ns.sleep(10)
+	await ns.sleep(0)
 
 
 }
+
+
+
+
 
 function buyNewServer(ns) {
 	if (ns.getServerMoneyAvailable('home') > ns.hacknet.getPurchaseNodeCost()) {
