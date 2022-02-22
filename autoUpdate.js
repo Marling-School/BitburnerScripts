a2921aea4d67f17fbd969930effe7d683dde4224
Karl Davis <karldavis@me.com>
2022-02-22 12:02:37 +0000

    Can now import scripts direct from repos.

---
+++ autoUpdate.js
@@ -0,0 +1,15 @@
+let config = { 
+    rootUrl: 'https://raw.githubusercontent.com/Deek1337/BitburnerScripts/main/'
+}
+export async function main(ns) {
+    while (true) {
+        let result = await ns.wget(`${config.rootUrl}import.js`, 'import.js');
+        if (result) {
+            ns.toast('Auto Update Completed');
+        } else {
+            ns.toast('Error updating import.js.');
+        }
+        await ns.sleep(120000)
+        
+    }
+}
---
+++ import.js
@@ -0,0 +1,54 @@
+let config = {
+  folder: 'scripts',
+  rootUrl: 'https://raw.githubusercontent.com/Deek1337/BitburnerScripts/main/'
+};
+
+export async function main(ns) {
+  let filesImported = await importFiles(ns);
+  ns.tprint('='.repeat(20));
+  if (filesImported) {
+    ns.tprint('Hey! Thank you for downloading the BitBurner Scripts.');
+    ns.tprint(`You've installed these in the ${config.folder} directory.`);
+  } else {
+    ns.tprint(
+      'You had some issues downloading files, please reach out to the repo maintainer or check your config.'
+    );
+  }
+}
+
+async function importFiles(ns) {
+  let rootFiles = [
+    'autoUpdate.js',
+    'utils.js'
+  ]
+  let files = [
+    'dashboard.js',
+    'hack.js',
+    'serverStatus.js',
+    'hacknetBoot.js',
+    'scriptPush.js'
+  ];
+
+  let filesImported = true;
+  for (let file of rootFiles) {
+    let remoteFileName = `${config.rootUrl}${file}`;
+    let result = await ns.wget(remoteFileName, `${file}`);
+    filesImported = filesImported && result;
+    ns.tprint(`Top Level File: ${file}: ${result ? '✔️' : '❌'}`);
+  }
+  for (let file of files) {
+    let remoteFileName = `${config.rootUrl}scripts/${file}`;
+    let result = await ns.wget(remoteFileName, `/${getFolder()}/${file}`);
+    filesImported = filesImported && result;
+    ns.tprint(`File: ${file}: ${result ? '✔️' : '❌'}`);
+  }
+  return filesImported;
+}
+
+export function getFolder() {
+  return config.folder;
+}
+
+export function getHackScript() {
+  return `/${getFolder()}/hack.js`;
+}
