e297429229a5dead15eb095b1f4f2c77ee217e17
Karl Davis <karldavis@me.com>
2022-02-22 12:11:02 +0000

    moved to utils or elsewhere

--- hacknet/sellallhashes.js
+++
@@ -1,8 +0,0 @@
-/** @param {NS} ns **/
-export async function main(ns) {
-	for(let i = 0; i<ns.hacknet.numHashes();++i) {
-		ns.hacknet.spendHashes("Sell for Money");
-	}
-	await ns.sleep(100)
-	
-}
